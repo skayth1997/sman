988e82e0606a3f424d9be2ba2aa13ef9de5da748
Tigran-105 <tik.sargsyan105@gmail.com>
2022-04-21 17:23:27 +0400

    change use formatter and test names, create new function for render

--- components/elements/input/__test__/input.test.tsx
+++ components/elements/input/__test__/input.test.tsx
@@ -1,35 +1,42 @@
 /** @jest-environment jsdom */
-import {render, screen, fireEvent} from '@testing-library/react';
-import "@testing-library/jest-dom"
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Input from "../input";
-import {ThemeProvider} from "styled-components";
+import { ThemeProvider } from "styled-components";
 import theme from "../../../../lib/theme";
 
+const renderForInput = (label, placeholder) => {
+  return render(<Input />, {
+    wrapper: () => (
+      <ThemeProvider
+        theme={theme}
+        children={<Input label={label} placeholder={placeholder} />}
+      />
+    ),
+  });
+};
+
+let inputElement;
 
 describe("input", () => {
-
-  it('input placeholder test', () => {
-    render(<Input/>, {
-      wrapper: () => <ThemeProvider theme={theme} children={<Input/>}/>
-    });
-    const inputElement = screen.getByPlaceholderText("write now");
+  renderForInput("label", "placeholder");
+  it("should be able to pass input through placeholder", () => {
+    inputElement = screen.getByPlaceholderText("placeholder");
     expect(inputElement).toBeInTheDocument();
   });
 
-  it('input change text', () => {
-    render(<Input/>, {
-      wrapper: () => <ThemeProvider theme={theme} children={<Input/>}/>
+  it("should be able to type into input", () => {
+    renderForInput("label", "placeholder");
+    inputElement = screen.getByPlaceholderText("placeholder");
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
     });
-    const inputElement = screen.getByPlaceholderText("write now");
-    fireEvent.change(inputElement, {target: {value: "Go Grocery Shopping"}})
     expect(inputElement).toHaveDisplayValue("Go Grocery Shopping");
   });
 
-  it('input label test',  () => {
-    render(<Input/>, {
-      wrapper:() =><ThemeProvider theme={theme} children={<Input/>}/>
-    })
-    const inputElement = screen.getByLabelText("Full name")
-    expect(inputElement).toBeInTheDocument()
+  it("should be able to pass input through label", () => {
+    renderForInput("label", "placeholder");
+    inputElement = screen.getByLabelText("label");
+    expect(inputElement).toBeInTheDocument();
   });
-})
+});
--- components/elements/input/input.tsx
+++ components/elements/input/input.tsx
@@ -1,12 +1,16 @@
-import {FunctionComponent} from "react";
+import { FunctionComponent } from "react";
 import * as S from "./input.styled";
 import { InputProps } from "./input.types";
 
-const Input: FunctionComponent<InputProps> = () => {
+const Input: FunctionComponent<InputProps> = ({ placeholder, label }) => {
   return (
     <S.InputWrapper>
-      <input id={"input"} type="text" placeholder={"write now"} required />
-      <label htmlFor={"input"}>Full name</label>
+      <input
+        aria-label={label}
+        type="text"
+        placeholder={placeholder}
+        required
+      />
     </S.InputWrapper>
   );
 };
