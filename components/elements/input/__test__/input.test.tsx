a4ccc07b716913827f276b0d10dd678c448cb7ad
Tigran-105 <tik.sargsyan105@gmail.com>
2022-04-22 14:07:42 +0400

    change test names and create new test

--- components/elements/input/__test__/input.test.tsx
+++ components/elements/input/__test__/input.test.tsx
@@ -5,7 +5,7 @@ import Input from "../input";
 import { ThemeProvider } from "styled-components";
 import theme from "../../../../lib/theme";
 
-const renderForInput = (label, placeholder) => {
+const renderInput = (label, placeholder) => {
   return render(<Input />, {
     wrapper: () => (
       <ThemeProvider
@@ -16,27 +16,35 @@ const renderForInput = (label, placeholder) => {
   });
 };
 
-let inputElement;
-
-describe("input", () => {
-  renderForInput("label", "placeholder");
-  it("should be able to pass input through placeholder", () => {
-    inputElement = screen.getByPlaceholderText("placeholder");
+describe("Input", () => {
+  renderInput("label", "placeholder");
+  it("should be input", () => {
+    const inputElement = screen.getByPlaceholderText("placeholder");
     expect(inputElement).toBeInTheDocument();
   });
 
+  it("should be label", () => {
+    renderInput("label", "placeholder");
+    const inputElement = screen.getByLabelText("label");
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("focus on the input when label is clicked", async () => {
+    renderInput("label", "placeholder");
+    const labelElement = screen.getByLabelText("label");
+    const inputElement = screen.getByPlaceholderText("placeholder");
+    fireEvent.click(labelElement);
+    await (() => {
+      expect(document.activeElement).toBe(inputElement);
+    });
+  });
+
   it("should be able to type into input", () => {
-    renderForInput("label", "placeholder");
-    inputElement = screen.getByPlaceholderText("placeholder");
+    renderInput("label", "placeholder");
+    const inputElement = screen.getByPlaceholderText("placeholder");
     fireEvent.change(inputElement, {
       target: { value: "Go Grocery Shopping" },
     });
     expect(inputElement).toHaveDisplayValue("Go Grocery Shopping");
-  });
-
-  it("should be able to pass input through label", () => {
-    renderForInput("label", "placeholder");
-    inputElement = screen.getByLabelText("label");
-    expect(inputElement).toBeInTheDocument();
   });
 });
--- components/elements/input/input.tsx
+++ components/elements/input/input.tsx
@@ -1,16 +1,17 @@
 import { FunctionComponent } from "react";
 import * as S from "./input.styled";
 import { InputProps } from "./input.types";
-
 const Input: FunctionComponent<InputProps> = ({ placeholder, label }) => {
   return (
     <S.InputWrapper>
-      <input
-        aria-label={label}
-        type="text"
-        placeholder={placeholder}
-        required
-      />
+      {label ? (
+        <label>
+          {label}
+          <input type="text" required placeholder={placeholder} />
+        </label>
+      ) : (
+        <input type="text" required placeholder={placeholder} />
+      )}
     </S.InputWrapper>
   );
 };
